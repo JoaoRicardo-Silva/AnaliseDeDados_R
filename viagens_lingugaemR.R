f1ebfc9485912371c94c333d40904fccd1f502a8
J. Ricardo Silva <38896158+JoaoRicardo-Silva@users.noreply.github.com>
2025-05-06 07:57:41 -0300

    Update Project

--- viagens_lingugaemR.R
+++ viagens_lingugaemR.R
@@ -131,3 +131,45 @@ ggplot(p1, aes(x = reorder(orgao, valor), y = valor))+
   geom_bar(stat = "identity")+
   coord_flip()+
   labs(x = "Valor", y = "Órgãos")
+
+# 2. Qual é o valor gasto por cidade?
+
+#Criando um dataframe com os 15 cidades que gastam mais
+
+p2 <- viagens %>%
+    group_by (Destinos) %>%
+    summarise (n = sum(Valor.passagens)) %>%
+    arrange (desc(n)) %>%
+    top_n(15)
+
+p2
+
+#alterando o nome das colunas
+names (p2) <- c ("destino", "valor")
+p2
+
+#Criando o gráfico
+ggplot(p2, aes(x = reorder(destino, valor), y = valor))+
+  geom_bar(stat = "identity", fill = "#0ba791")+
+  geom_text(aes(label = valor), vjust = 0.3, size = 3)+
+  coord_flip()+
+  labs(x = "Valor", y = "Destino")
+
+#options(scipen = 999)
+
+# 3. Qual é a quantidade de viagens por mês?
+
+#Crinado um dataframe com a quantidade de viagens por Ano/mês
+p3 <- viagens %>%
+    group_by (data.inicio.formatada) %>%
+    summarise (qtd = n_distinct(Identificador.do.processo.de.viagem))
+
+head (p3)
+
+#Crinado o gráfico
+ggplot (p3, aes(x = data.inicio.formatada, y = qtd, group = 1))+
+    geom_line()+
+    geom_point()
+
+#Relembrando que todas as imagens plot geradas pelo ggplot estaram disponiveis neste projeto na pasta "img"
+
